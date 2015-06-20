bd016acd7921a9aeb23e4c1f3c99337fb12601e5
Brian DiChiara <briandichiara@gmail.com>
2015-06-20 07:49:17 -0500

    Added some comments, added a small condition to query

--- application.php
+++ application.php
@@ -5,13 +5,20 @@ if( ! headers_sent() && ! isset( $_SESSION ) ){
 	session_start();
 }
 
+/**
+ * Create a new PHP Data Object in the global space
+ */
 global $db;
 $db = new PDO(
-    'mysql:host=localhost;dbname=ags_todo;charset=utf8',
-    'root',
-    'root'
+    'mysql:host=localhost;dbname=ags_todo;charset=utf8', // host, dbname, and charset
+    'root', // username
+    'root'  // password
 );
 
+/**
+ * Fetch To Do items from todo_items table
+ * @return mixed Array of Item Objects or false
+ */
 function get_todo_items(){
 	global $db;
 
@@ -19,7 +26,9 @@ function get_todo_items(){
 
 	try {
 		$query = $db->query( 'SELECT * FROM todo_items WHERE `deleted` IS NULL ORDER BY `added` ASC' );
-		$items = $query->fetchAll( PDO::FETCH_OBJ );
+		if( $query ){
+			$items = $query->fetchAll( PDO::FETCH_OBJ );
+		}
 	} catch( PDOException $e ){
 		echo $e->getMessage();
 		exit();
@@ -31,13 +40,17 @@ function get_todo_items(){
 	return false;
 }
 
+/**
+ * Display an ordered list of the To Do items
+ * @return void
+ */
 function output_todo_list(){
-	echo '<ul id="todo-list">';
+	echo '<ol id="todo-list">';
 		foreach( get_todo_items() as $item ): ?>
 			<li<?php if( $item->done ) echo ' class="done"'; ?>>
 				<input class="tick" type="checkbox" value="<?php echo $item->id; ?>" <?php if( $item->done ) echo ' checked="checked"'; ?> />
 				<span><?php echo $item->item; ?></span>
 				<a href="#remove" class="remove">x</a></li>
 		<?php endforeach;
-	echo '</ul>';
+	echo '</ol>';
 }
